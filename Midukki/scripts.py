227ad3a82f06ae4f26c4912474b383b776005698
AazIbrm <97102321+AazIbrm@users.noreply.github.com>
2022-12-31 06:50:46 +0530

    Update scripts.py

--- Midukki/scripts.py
+++ Midukki/scripts.py
@@ -27,17 +27,11 @@ ABOUT_TXT = """
 [{name}](t.me/{username}) Was created on September 4, 2022
 We are currently developing this bot, using only the Pyrogram library.
 credits to muhammed and motech Channel
-
 ➾ Developers : @Thisis_sanu
 ➾ Language : Python3
 ➾ Framework : Pyrogram
 ➾ Database : Mongo dub
 ➾ Server : Railway
-
-DISCLAIMER NOTE‼️:
-- @Sanufilterv1_bot is an Open Source Project. All the files in this bot are freely available on the internet or posted by somebody else. This bot is indexing files which are already uploaded on Telegram for easy of searching, We respect all the copyright laws and works in compliance with DMCA and EUCD. If anything is against law please contact us so that it can be removed asap. It is forbidden to download, stream, reproduce, or by any means, share, or consume, content without explicit permission from the content creator or legal copyright holder. If you believe this bot is violating your intellectual property, contact the respective channels for removal. The Bot does not own any of these contents, it only index the files from telegram. 
-
-For any content removal contact admin it will be remove instantly
 """
 
 DONATE_TXT = """
